82e125ccf02e7b91f5f8b3ac5234601d5f09506f
MatthieuBoeykens <matthieu.boeykens@gmail.com>
2015-01-07 11:37:55 +0100

    Création de la route /login vers /admin

--- src/ESN/LoginBundle/Controller/DefaultController.php
+++ src/ESN/LoginBundle/Controller/DefaultController.php
@@ -2,12 +2,23 @@
 
 namespace ESN\LoginBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        // Gestion d'un formulaire :
+        // Si la requête est un post, c'est uqe le visiteur a soumis le formulaire
+        if ($request->isMethod('POST')) {
+            //Puis on redirige vers la page de visualisation de cette annonce
+            return $this->redirect($this->generateUrl('esn_admin_homepage'));
+        }
+        // Si on n'est pas en Post, alors on affiche le formulaire
         return $this->render('ESNLoginBundle:Default:index.html.twig');
     }
+    
+    
 }
