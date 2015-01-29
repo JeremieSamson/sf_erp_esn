1f23cf67da2ef55dbad8e5802507c0191f1fe9b7
Donatien Thorez <donatienthorez@gmail.com>
2015-01-29 22:02:55 +0100

    Ajout des routes et templates pour le sous menu Association de l'onglet HR

--- src/ESN/HRBundle/Controller/HRController.php
+++ src/ESN/HRBundle/Controller/HRController.php
@@ -14,11 +14,11 @@ class HRController extends Controller
     
     public function associationAction()
     {
-        return $this->render('ESNHRBundle:HR:association.html.twig');  
+        return $this->render('ESNHRBundle:Association:index.html.twig');  
     }
     
     public function recruitmentAction()
     {
-        return $this->render('ESNHRBundle:HR:recruitment.html.twig');
+        return $this->render('ESNHRBundle:Recruitment:index.html.twig');
     }
 }
