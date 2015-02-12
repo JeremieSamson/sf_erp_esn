80a7590b4dac220e45170af5dc5e2851b3efe0b8
Donatien Thorez <donatienthorez@gmail.com>
2015-02-12 11:19:44 +0100

    Ajout du templates et des routes de l'onglet Treasury

--- src/ESN/TreasuryBundle/Controller/TreasuryController.php
+++ src/ESN/TreasuryBundle/Controller/TreasuryController.php
@@ -6,10 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TreasuryController extends Controller
 {
-    public function indexAction()
+     public function indexAction($type)
     {
-        return $this->render('ESNTreasuryBundle::index.html.twig', array(
+        $data = array('title' => "Treasury", 'type' => $type);
+        return $this->render('ESNTreasuryBundle::index.html.twig', $data);
+    }
+    public function seeAction()
+    {
+        return $this->render('ESNTreasuryBundle:Operations:see.html.twig', array(
             'title' => "Treasury"
         ));
     }
+    public function addAction()
+    {
+        return $this->render('ESNTreasuryBundle:Operations:add.html.twig', array(
+            'title' => "Treasury"
+        ));
+    }
+    public function listAction()
+    {
+        return $this->render('ESNTreasuryBundle:Operations:list.html.twig', array(
+            'title' => "Treasury"
+        ));
+    }
+    
 }
