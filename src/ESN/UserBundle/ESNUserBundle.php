a661277a3b24999c76c7168b799190b541e7b571
Jeremie Samson <jeremie.samson76@gmail.com>
2015-10-13 22:51:17 +0200

    Add FOSUserBundle

---
+++ src/ESN/UserBundle/ESNUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ESN\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ESNUserBundle extends Bundle
+{
+}
---
+++ src/ESN/UserBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ESN\UserBundle\User;
+
+/**
+ * Created by PhpStorm.
+ * User: ylly
+ * Date: 13/10/15
+ * Time: 22:44
+ */
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="User")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
